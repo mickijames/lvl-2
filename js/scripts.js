ffd9b885328014e67544de6fa793669883c74945
mickijames <micalynjeanette@eagles.ewu.edu>
2017-06-15 13:09:33 -0700

    Displayed time on page
    * displays without a zero if minutes is a single digit

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,35 @@ function setDate() {
     const now = new Date();
 
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     
-    // Display digital clock
+    rotateHands(seconds, mins, hour);
+    displayDigital(mins, hour);
 //    const template = `<div> ${hour} + ':' + ${mins} </div>`;
 //    const div = document.createElement('div');
+}
+
+function rotateHands(seconds, mins, hour) {
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+function displayDigital(mins, hour) {
+    var newElem = document.createElement('h2');
+    var content = document.createTextNode(`${hour}:${mins}`);
+    newElem.appendChild(content);
+    
+    console.log(newElem);
+    
+    var parent = document.getElementById('digital');
+    parent.replaceChild(newElem, parent.firstElementChild);
 }
 
 setInterval(setDate, 1000);
